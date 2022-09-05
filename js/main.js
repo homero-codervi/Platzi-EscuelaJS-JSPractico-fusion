4989878810aa2470a976e94a8757a6e07e05eeca
Homero Villanueva <homero.codervi@gmail.com>
2022-09-05 16:43:22 -0500

    Menu mobile added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 
 const emailObject = document.querySelector('.navbar-email');
+const menuBurguer = document.querySelector('.menu');
 const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
 const INACTIVE = 'inactive';
 
-console.log(emailObject);
 emailObject.addEventListener('click', toggleDesktopMenu);
+menuBurguer.addEventListener('click', toggleMobileMenu);
 
-function toggleDesktopMenu(event){
-    desktopMenu.classList.toggle(INACTIVE)
-    console.log("Click on the email");
+function toggleDesktopMenu( event ){
+    desktopMenu.classList.toggle( INACTIVE );
+    
+}
+
+function toggleMobileMenu( event ){
+    console.log( "Click on the Burguer" );
+    let result = mobileMenu.classList.toggle( INACTIVE );
+    
 }
